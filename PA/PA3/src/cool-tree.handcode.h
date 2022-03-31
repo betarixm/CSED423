7c3a12127c3ff16de588982444813489619b9546
beta <beta.underalpha@gmail.com>
2022-04-01 00:05:40 +0900

    (#6) PA3: reformat code

--- PA/PA3/src/cool-tree.handcode.h
+++ PA/PA3/src/cool-tree.handcode.h
@@ -9,30 +9,46 @@
 #include "cool.h"
 #include "stringtab.h"
 #include "tree.h"
+
 #define yylineno curr_lineno;
 extern int yylineno;
 
 inline Boolean copy_Boolean(Boolean b) { return b; }
+
 inline void assert_Boolean(Boolean) {}
+
 inline void dump_Boolean(ostream &stream, int padding, Boolean b) {
-    stream << pad(padding) << (int)b << "\n";
+    stream << pad(padding) << (int) b << "\n";
 }
 
 void dump_Symbol(ostream &stream, int padding, Symbol b);
+
 void assert_Symbol(Symbol b);
+
 Symbol copy_Symbol(Symbol b);
 
 class Program_class;
+
 typedef Program_class *Program;
+
 class Class__class;
+
 typedef Class__class *Class_;
+
 class Feature_class;
+
 typedef Feature_class *Feature;
+
 class Formal_class;
+
 typedef Formal_class *Formal;
+
 class Expression_class;
+
 typedef Expression_class *Expression;
+
 class Case_class;
+
 typedef Case_class *Case;
 
 typedef list_node<Class_> Classes_class;
