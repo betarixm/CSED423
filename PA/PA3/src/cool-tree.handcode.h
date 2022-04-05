6f66f4df182c75f6eb3ff9f25f797be4e1c21724
beta <beta.underalpha@gmail.com>
2022-04-06 01:00:43 +0900

    (#6) PA3: draft

--- PA/PA3/src/cool-tree.handcode.h
+++ PA/PA3/src/cool-tree.handcode.h
@@ -9,30 +9,49 @@
 #include "cool.h"
 #include "stringtab.h"
 #include "tree.h"
+#include "symtab.h"
+
 #define yylineno curr_lineno;
 extern int yylineno;
 
+class ClassTable;
+
 inline Boolean copy_Boolean(Boolean b) { return b; }
+
 inline void assert_Boolean(Boolean) {}
+
 inline void dump_Boolean(ostream &stream, int padding, Boolean b) {
-    stream << pad(padding) << (int)b << "\n";
+    stream << pad(padding) << (int) b << "\n";
 }
 
 void dump_Symbol(ostream &stream, int padding, Symbol b);
+
 void assert_Symbol(Symbol b);
+
 Symbol copy_Symbol(Symbol b);
 
 class Program_class;
+
 typedef Program_class *Program;
+
 class Class__class;
+
 typedef Class__class *Class_;
+
 class Feature_class;
+
 typedef Feature_class *Feature;
+
 class Formal_class;
+
 typedef Formal_class *Formal;
+
 class Expression_class;
+
 typedef Expression_class *Expression;
+
 class Case_class;
+
 typedef Case_class *Case;
 
 typedef list_node<Class_> Classes_class;
@@ -58,27 +77,73 @@ typedef Cases_class *Cases;
     virtual Symbol get_filename() = 0;                \
     virtual void dump_with_types(ostream &, int) = 0; \
     virtual Symbol get_name() = 0;                    \
-    virtual Symbol get_parent() = 0;
+    virtual Symbol get_parent() = 0;                  \
+    virtual Features get_features() = 0;
+
 
 #define class__EXTRAS                          \
     Symbol get_filename() { return filename; } \
     void dump_with_types(ostream &, int);      \
     Symbol get_name() { return name; }         \
-    Symbol get_parent() { return parent; }
+    Symbol get_parent() { return parent; }     \
+    Features get_features()  { return features; }
 
-#define Feature_EXTRAS virtual void dump_with_types(ostream &, int) = 0;
+
+#define Feature_EXTRAS \
+    virtual bool is_attr() = 0; \
+    virtual void dump_with_types(ostream &, int) = 0; \
+    virtual bool is_method() = 0;                       \
+
 
 #define Feature_SHARED_EXTRAS void dump_with_types(ostream &, int);
 
-#define Formal_EXTRAS virtual void dump_with_types(ostream &, int) = 0;
+#define method_EXTRAS                            \
+    bool is_method() { return true; }                \
+    bool is_attr() { return false; }                 \
+    Symbol get_name() { return name; }               \
+    Formals get_formals() { return formals; }        \
+    Symbol get_return_type() { return return_type; } \
+    Expression get_body_expr() { return expr; }      \
+    Symbol check_type(cool::SymbolTable<Symbol, Symbol>* o, Class_ current_class, ClassTable *ct);
 
-#define formal_EXTRAS void dump_with_types(ostream &, int);
 
-#define Case_EXTRAS virtual void dump_with_types(ostream &, int) = 0;
+#define attr_EXTRAS                              \
+    bool is_method() { return false; }               \
+    bool is_attr() { return true; }                  \
+    Symbol get_name() { return name; }               \
+    Symbol get_type() { return type_decl; }          \
+    Expression get_init_expr() { return init; }      \
+    Symbol check_type(cool::SymbolTable<Symbol, Symbol>* o, Class_ current_class, ClassTable *ct);
 
-#define branch_EXTRAS void dump_with_types(ostream &, int);
 
-#define Expression_EXTRAS                             \
+#define Formal_EXTRAS                             \
+    virtual void dump_with_types(ostream &, int) = 0; \
+    virtual Symbol get_name() = 0;                    \
+    virtual Symbol get_type() = 0;
+
+#define formal_EXTRAS                 \
+    void dump_with_types(ostream &, int); \
+    Symbol get_name() {return name;}      \
+    Symbol get_type() {return type_decl;}
+
+#define Case_EXTRAS                                 \
+    Symbol type;                                        \
+    Symbol get_type() {return type;}                    \
+    void set_type(Symbol s){type =s;}                   \
+    virtual void dump_with_types(ostream &, int) = 0;   \
+    virtual Symbol check_type(cool::SymbolTable<Symbol, Symbol>* o, Class_ current_class, ClassTable *ct) = 0;
+
+#define branch_EXTRAS \
+    Symbol type;\
+    void dump_with_types(ostream &, int); \
+    Symbol get_type() { return type;}     \
+    void set_type(Symbol s) {type = s;}   \
+    Symbol get_type_decl() {return type_decl;} \
+    Symbol check_type(cool::SymbolTable<Symbol, Symbol>* o, Class_ current_class, ClassTable *ct);
+
+
+#define Expression_EXTRAS          \
+    virtual Symbol check_type(cool::SymbolTable<Symbol, Symbol>* o, Class_ current_class, ClassTable *ct) = 0;\
     Symbol type;                                      \
     Symbol get_type() { return type; }                \
     Expression set_type(Symbol s) {                   \
@@ -87,8 +152,11 @@ typedef Cases_class *Cases;
     }                                                 \
     virtual void dump_with_types(ostream &, int) = 0; \
     void dump_type(ostream &, int);                   \
-    Expression_class() { type = (Symbol)NULL; }
+    Expression_class() { type = (Symbol)NULL; }       \
 
-#define Expression_SHARED_EXTRAS void dump_with_types(ostream &, int);
+#define Expression_SHARED_EXTRAS \
+    Symbol check_type(cool::SymbolTable<Symbol, Symbol>* o, Class_ current_class, ClassTable *ct);\
+    void dump_with_types(ostream &, int); \
+
 
 #endif
--- PA/PA3/src/semant.h
+++ PA/PA3/src/semant.h
@@ -5,16 +5,25 @@
 
 #include <iostream>
 #include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
 
 #include "cool-tree.h"
 #include "list.h"
 #include "stringtab.h"
 #include "symtab.h"
+#include "list.h"
 
 #define TRUE 1
 #define FALSE 0
 
 class ClassTable;
+
+class method_class;
+
+class attr_class;
+
 typedef ClassTable *ClassTableP;
 
 // This is a structure that may be used to contain the semantic
@@ -23,21 +32,45 @@ typedef ClassTable *ClassTableP;
 // methods.
 
 class ClassTable {
-   private:
+private:
     int semant_errors;
-    std::map<Symbol, Class_> symbol_class_map;
+    std::map<Symbol, Class_> _symbol_class_map;
+    std::map<Symbol, std::map<Symbol, method_class *> *> _method_map;
+    std::map<Symbol, std::map<Symbol, attr_class *> *> _attr_map;
     std::map<Symbol, Symbol> parent_map;
+    cool::SymbolTable<Symbol, Symbol> _object_symtab;
     ostream &error_stream;
 
     void install_basic_classes();
-    void add_class(Class_ class_);
 
-   public:
+    void add_class(Symbol name, Class_ c);
+
+public:
     ClassTable(Classes);
+
     int errors() { return semant_errors; }
+
+    bool is_child(Symbol child, Symbol parent, Class_ current_class);
+
+    Symbol lca(Symbol c1, Symbol c2, Class_ current_class);
+
     ostream &semant_error();
+
     ostream &semant_error(Class_ c);
+
     ostream &semant_error(Symbol filename, tree_node *t);
+
+    std::map<Symbol, Symbol> *get_parent_map();
+
+    std::map<Symbol, Class_> *symbol_class_map();
+
+    std::map<Symbol, std::map<Symbol, method_class *> *> * method_map();
+
+    std::map<Symbol, std::map<Symbol, attr_class *> *> * attr_map();
+
+    cool::SymbolTable<Symbol, Symbol> *object_symtab();
+
+    method_class *get_method(Symbol class_name, Symbol method_name, Class_ current_class);
 };
 
 #endif
